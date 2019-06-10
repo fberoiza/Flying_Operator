68e963ee1540985bc8d7eb656fd7ecbc8a5a86ac
fberoiza <fberoiza@gmail.com>
2019-06-10 14:44:19 +0200

    FB: Commit Inicial

---
+++ src/main.java
@@ -0,0 +1,26 @@
+/**
+ * @Class       main
+ * @Date        10.06.2019, 14:25:43
+ * @author      Felipe Beroiza
+ * @Company     FIB
+ * @Description 
+ *
+ * Copyright(c) 2019 Felipe Beroiza, Inc. All Rights Reserved.
+ * This software is the proprietary information of Felipe Beroiza
+ */
+
+//Own Classes
+
+//Downloaded Classes
+
+//Java Native Classes
+
+public class main {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    System.out.println("Starting Program");
+    
+  }
+
+}
